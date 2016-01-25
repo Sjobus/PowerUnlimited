5120a74f5bc3982ff1960b3c8917fb4935145def
Sjobus <sibevanetten@hotmail.com>
2016-01-25 22:36:37 +0100

    Unitest project toegevoegd + 1 test
    Logintest werkt

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Mail;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerUnlimited.Classen;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            MailAddress mail = new MailAddress("Jan@jan.nl");
+            string naam = "JAN";
+            int verwachtAccountId = 22;
+            string verwachtAccountType = "redacteur";
+            Database.Instance.Login(mail, "QWERTY");
+            Assert.AreEqual(verwachtAccountId,Database.Instance.WebGebruiker.ID, "Webgebruiker Id incorrect.");
+            Assert.AreEqual(naam,Database.Instance.WebGebruiker.GebruikerNaam,"Gebruikernaam incorrect.");
+            Assert.AreEqual(verwachtAccountType,Database.Instance.WebGebruiker.AccountType,"AccountType incorrect.");
+
+        }
+    }
+}
